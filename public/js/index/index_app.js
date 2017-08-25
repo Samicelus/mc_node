9566e65251ecaa0fbf9484090db8480e7d89f48e
samicelus <samicelus@hotmail.com>
2017-08-25 15:42:23 +0800

    token

--- public/js/index/index_app.js
+++ public/js/index/index_app.js
@@ -24,12 +24,13 @@ indexApp.controller('mainCtrl', function ($scope, $http) {
         });
     }
     $scope.chgPwd = function(){
+        var that = this;
         $http({
             method: 'POST',
             url: 'http://119.23.73.86:8030/changePassword',
             data: {
-                old_password: $scope.old_password,
-                new_password: $scope.new_password
+                old_password: that.old_password,
+                new_password: that.new_password
             }
         }).then(function successCallback(response){
             console.log(response.data);
