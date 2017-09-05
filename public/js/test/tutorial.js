f97ed0f7d040a1d3e74e140d2700dd745153f113
samicelus <samicelus@hotmail.com>
2017-09-05 17:50:37 +0800

    test

--- public/js/test/tutorial.js
+++ public/js/test/tutorial.js
@@ -2,22 +2,6 @@
  * Created by Administrator on 2016/11/29.
  */
 window.markers = [];
-// 获取弹窗
-var modal = document.getElementById('myModal');
-// 获取 <span> 元素，用于关闭弹窗
-var span = document.getElementById('close');
-
-// 点击 <span> (x), 关闭弹窗
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// 在用户点击其他地方时，关闭弹窗
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
 //必须在服务器上才能看到效果！
 window.onload=function(){
@@ -112,7 +96,6 @@ function loadingAllImg(){
             circle: 20,
             tooltip: 'customer added marker'
         };
-        modal.style.display = "block";
         console.log(JSON.stringify(marker));
         add_marker("test","tutorial", JSON.stringify(marker),function(){
             PSV.clearMarkers();
