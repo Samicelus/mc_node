ddc78edad799ac2ec392c7d7c6110228e8e04297
samicelus <samicelus@hotmail.com>
2017-09-05 16:19:16 +0800

    test

--- public/js/test/tutorial.js
+++ public/js/test/tutorial.js
@@ -85,6 +85,6 @@ function loadingAllImg(){
             circle: 20,
             tooltip: 'customer added marker'
         }
-        add_marker(page_name,panorama_id, marker);
+        add_marker("test","tutorial", marker);
     });
 }
