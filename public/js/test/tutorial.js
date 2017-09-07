bb9b79e54eac7d20a1bb6d57f2a36e85c074f2db
samicelus <samicelus@hotmail.com>
2017-09-07 11:42:31 +0800

    navbar

--- public/js/test/tutorial.js
+++ public/js/test/tutorial.js
@@ -93,7 +93,7 @@ function loadingAllImg(){
      * Create a new marker when the user clicks somewhere
      */
     PSV.on('click', function(e) {
-        var marker_name = document.getElementById("marker_name");
+        var marker_name = document.getElementById("marker_name").value;
         console.log(marker_name);
         var marker = {
             id: '#' + Math.random(),
