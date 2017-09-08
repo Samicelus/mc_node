a0b678e5b9fdc91dbd6b02a10a0e0f8b663f8aaa
samicelus <samicelus@hotmail.com>
2017-09-08 17:49:42 +0800

    dialog css

--- public/js/test/tutorial.js
+++ public/js/test/tutorial.js
@@ -116,8 +116,6 @@ function loadingAllImg(){
         panorama: '../images/tutorial.jpg',
 
         markers: window.markers,
-        min_fov:1,
-        max_fov:179,
         // 放全景图的元素
         container: div,
 
