72c591dcb2704d161efdc899df282ae584684c5e
samicelus <samicelus@hotmail.com>
2017-10-08 11:20:37 +0800

    toggle_button

--- public/js/test/tutorial.js
+++ public/js/test/tutorial.js
@@ -337,8 +337,10 @@ function loadingAllImg(ret_env){
                 var sendData = {
                     page_id: window.page_id,
                     current_position:JSON.stringify(window.position),
-                    move: {longitude:window.longitude, latitude:window.latitude}
+                    init_position: {}
                 };
+                sendData.init_position.longitude = window.longitude;
+                sendData.init_position.latitude = window.latitude;
                 $.post("/panorama/setInitPosition",sendData,function(data,status){
                     window.set_init = false;
                     toggle_button_style($("#justify_init"));
