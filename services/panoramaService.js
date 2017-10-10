ebdab7678721318e2d85fa0f06428695916c7ac7
samicelus <samicelus@hotmail.com>
2017-10-10 15:36:06 +0800

    tencent cos

--- services/panoramaService.js
+++ services/panoramaService.js
@@ -22,7 +22,7 @@ service.getObj = function (req, res) {
     let file_name = req.params.filename;
     let options = {
         Bucket : 'mcpanorama', /* 必须 */
-        Region : 'cd', /* 必须 */
+        Region : 'ap-chengdu', /* 必须 */
         Key : file_name
     };
     cos.getObjectAsync(options).then(function(data) {
@@ -38,7 +38,7 @@ function upload_one_file(path, name){
     return fs.readFileAsync(path).then(function(data) {
         let options = {
             Bucket: 'mcpanorama', /* 必须 */
-            Region: 'cd', /* 必须 */
+            Region: 'ap-chengdu', /* 必须 */
             Key: name, /* 必须 */
             contentLength: data.length,
             Body: data
