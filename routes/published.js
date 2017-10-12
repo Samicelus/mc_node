7610b696125bcbc4fb86545e1ef0b95acc37ce03
samicelus <samicelus@hotmail.com>
2017-10-12 13:52:26 +0800

    published

--- routes/published.js
+++ routes/published.js
@@ -5,7 +5,7 @@ var handler = require('../services/panoramaService.js');
 
 module.exports = function(app){
     app.route('/panoramaPub/:page_id').get(function (req, res) {
-        res.render('../public/published/published.ejs',{page_id:req.params.page_id});
+        res.render('../public/published/published',{page_id:req.params.page_id});
     });
 };
 
