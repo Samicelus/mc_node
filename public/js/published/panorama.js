d49a19fdf21dc16f9484c74bd2ae69b1b19a3551
samicelus <samicelus@hotmail.com>
2017-10-13 17:09:07 +0800

    share_weixin

--- public/js/published/panorama.js
+++ public/js/published/panorama.js
@@ -21,6 +21,8 @@ function getTitleHeight(){
     var titleHeight=parseFloat(getComputedStyle(title).height);
     var maxHeight=window.innerHeight;
     window.canvasHeight=parseFloat(maxHeight-titleHeight)+'px';
+    $(".btn-audio").css('top',parseFloat(titleHeight-5)+'px');
+    $(".btn-audio").css('right',parseFloat(titleHeight-5)+'px');
 }
 
 function getDefaultPage(){
