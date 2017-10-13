433ad779c5e182a2ddda3eb15a57f901c08074d5
samicelus <samicelus@hotmail.com>
2017-10-13 16:40:13 +0800

    share_weixin

--- public/js/published/panorama.js
+++ public/js/published/panorama.js
@@ -155,7 +155,7 @@ function changeTitle(ret_env){
 
 $('#page_audio').on('ended', function() {
     console.log("音频已播放完成");
-    $('.btn-audio').css({'background':'url(images/music.png) no-repeat center bottom','background-size':'cover'});
+    $('.btn-audio').css({'background':'url(../../images/music.png) no-repeat center bottom','background-size':'cover'});
 })
 //播放器控制
 var audio = document.getElementById('page_audio');
@@ -163,11 +163,11 @@ audio.volume = .3;
 $('.btn-audio').click(function() {
     event.stopPropagation();//防止冒泡
     if(audio.paused){ //如果当前是暂停状态
-        $('.btn-audio').css({'background':'url(images/music.png) no-repeat center bottom','background-size':'cover'});
+        $('.btn-audio').css({'background':'url(../../images/music.png) no-repeat center bottom','background-size':'cover'});
         audio.play(); //播放
         return;
     }else{//当前是播放状态
-        $('.btn-audio').css({'background':'url(images/music.png) no-repeat center bottom','background-size':'cover'});
+        $('.btn-audio').css({'background':'url(../../images/music.png) no-repeat center bottom','background-size':'cover'});
         audio.pause(); //暂停
     }
 });
