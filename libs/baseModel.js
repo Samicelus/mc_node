1cf20306149631f6bcf93cb18f83f9c7df3ae488
samicelus <samicelus@hotmail.com>
2017-11-09 18:10:13 +0800

    mongo test

--- libs/baseModel.js
+++ libs/baseModel.js
@@ -1,6 +1,5 @@
 'use strict';
-var Promise = require('bluebird');
-var mongoose = Promise.promisifyAll(require("mongoose"));
+var mongoose = require("./mongodb.js");
 class BaseModel{
     constructor() {
         this.mongoose = mongoose;
